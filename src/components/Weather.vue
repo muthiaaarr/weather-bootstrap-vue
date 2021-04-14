28637c31acb8b9d90eab8d1869212e57c6a01fe8
Muthia Rahma <muthiaarahmaa@gmail.com>
2021-04-14 10:05:11 +0700

    integrasi api

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -6,17 +6,16 @@
           <b-form class="search-bar">
             <label class="sr-only" for="inline-form-input-name">Name</label>
             <b-input-group size="lg">
-              <b-input-group-prepend is-text>
-                <b-icon icon="search"></b-icon>
-              </b-input-group-prepend>
               <b-form-input
                 id="inline-form-input-name"
                 class="form-input"
                 placeholder="Search..."
                 v-model="query"
-                @keypress="runApi(e)"
               >
               </b-form-input>
+              <b-button variant="primary" size="lg" @click="runApi">
+                <b-icon icon="search"></b-icon>
+              </b-button>
             </b-input-group>
           </b-form>
         </b-col>
@@ -24,12 +23,15 @@
       <div class="detail">
         <b-row class="text-center">
           <b-col>
-            <h3 class="location">Bandung, Indonesia</h3>
+            <h3 class="location" v-if="this.city !== ''">
+              {{ this.city }}, {{ this.country }}
+            </h3>
+            <h3 class="location" v-else>Select your city/country</h3>
           </b-col>
         </b-row>
         <b-row class="text-center">
           <b-col>
-            <h3 class="time">{{dateFormat()}}</h3>
+            <h3 class="time">{{ dateFormat() }}</h3>
           </b-col>
         </b-row>
         <b-row class="text-center">
@@ -39,13 +41,24 @@
               variant="outline-info"
               size="lg"
               class="button-temp"
-              ><span class="temp">9°C</span></b-button
             >
+              <b-img
+                alt="weather"
+                :src="urlImage(this.icon)"
+                v-if="this.getIcon === true"
+              ></b-img>
+              <span class="temp" v-if="this.temperature > 0">
+                {{ this.temperatureFormat(this.temperature) }}°C
+              </span>
+              <span class="temp" v-else>0°C</span>
+            </b-button>
           </b-col>
         </b-row>
         <b-row class="text-center">
           <b-col>
-            <h3 class="weather2">Rain</h3>
+            <h3 class="weather2" v-if="this.weather !== ''">
+              {{ this.weatherDescription.toUpperCase() }}
+            </h3>
           </b-col>
         </b-row>
       </div>
@@ -54,7 +67,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "Weather",
@@ -62,33 +75,78 @@ export default {
     return {
       api_key: "e12fa74525c1b00e0a3c27e1d46f156a",
       url_base: "http://api.openweathermap.org/data/2.5/",
-      cors: 'https://cors-anywhere.herokuapp.com/',
+      cors: "https://cors-anywhere.herokuapp.com/",
       query: "",
-      weather: {},
+      city: "",
+      country: "",
+      temperature: 0,
+      weather: "",
+      weatherDescription: "",
+      icon: "",
+      getIcon: false,
+      mainProps: {
+        blank: true,
+        blankColor: "#777",
+        width: 75,
+        height: 75,
+        class: "m1",
+      },
     };
   },
   methods: {
-      runApi(e) {
-        const url = `${this.cors}${this.url_base}weather?q=${this.query}&APPID=${this.api_key}`
-        if (e.key === 'Enter') {              
-            axios.get(url)
-            .then(res => {
-                console.log(res.data)
-            })
-        }
-      },
-      dateFormat() {
-          let today = new Date()
-          const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-          const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    runApi() {
+      const url = `${this.cors}${this.url_base}weather?q=${this.query}&APPID=${this.api_key}`;
+      axios.get(url).then((res) => {
+        this.city = res.data.name;
+        this.country = res.data.sys.country;
+        this.temperature = res.data.main.temp;
+        this.weather = res.data.weather[0].main;
+        this.weatherDescription = res.data.weather[0].description;
+        this.icon = res.data.weather[0].icon;
+        this.getIcon = true;
+      });
+    },
+    dateFormat() {
+      let today = new Date();
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      const days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ];
 
-          let day = days[today.getDay()]
-          let date = today.getDate()
-          let month = months[today.getMonth()]
-          let year = today.getFullYear()
+      let day = days[today.getDay()];
+      let date = today.getDate();
+      let month = months[today.getMonth()];
+      let year = today.getFullYear();
 
-          return `${day}, ${month} ${date}, ${year}`
-      },
+      return `${day}, ${month} ${date}, ${year}`;
+    },
+    temperatureFormat(temperature) {
+      // Kelvin to Celcius
+      return Math.round(temperature - 273.15);
+    },
+    urlImage(icon) {
+      if (this.getIcon === true)
+        return `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    },
   },
 };
 </script>
@@ -107,6 +165,7 @@ export default {
 .form-input {
   border-radius: 10px;
   font-size: xx-large;
+  outline: none;
 }
 
 h1 {
@@ -137,9 +196,9 @@ h6 {
 }
 
 .button-temp {
-    border-radius: 10px;
-    width: 30%;
-    margin-top: 3%;
+  border-radius: 10px;
+  width: 20%;
+  margin-top: 3%;
 }
 
 .temp {
@@ -150,7 +209,6 @@ h6 {
 .weather2 {
   color: azure;
   font-size: 40px;
-  font-style: italic;
   margin-top: 3%;
 }
 </style>
